94e74e4488d9726a0b8e96a99dc4a804343aa30d
Ladislav Prix <ladislav.prix@designeo.cz>
2021-12-16 23:17:19 +0100

    Lint & format

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { ThemeProvider } from 'next-themes';
 import type { AppProps } from 'next/app';
 import Navbar from '../components/navbar';
-import '../../styles/globals.css'
+import '../../styles/globals.css';
 
 const App = ({ Component, pageProps }: AppProps) => (
   <ThemeProvider attribute="class" enableSystem={false} defaultTheme="dark">

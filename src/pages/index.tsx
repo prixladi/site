af2062839c4ce42ef3d8d879a02f2e8778d7ee0a
Ladislav Prix <ladislav.prix@designeo.cz>
2021-12-16 23:14:37 +0100

    Footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,11 +8,12 @@ import {
   scaleUpVariants,
   scaleUpTinyWhileHoverVariants,
 } from '../utils/motions';
+import Link from 'next/link';
 
 const Home: NextPage = () => (
   <>
     <PageSEO title="Ladislav Prix" />
-    <div className="max-w-7xl m-auto pt-8 md:pt-16 lg:pt-24 xl:pt-32 px-4 md:px-10">
+    <div className="max-w-7xl m-auto pt-8 md:pt-16 lg:pt-24 xl:pt-32 px-4 md:px-10 pb-36">
       <main className="flex flex-col lg:flex-row flex-1 gap-8 items-center">
         <motion.div
           initial="initial"
@@ -36,7 +37,11 @@ const Home: NextPage = () => (
               className="text-xl lg:pl-2 md:text-4xl font-semibold leading-tight md:leading-[3rem] text-gray-500 dark:text-gray-400 duration-700"
             >
               I&apos;m a 24 years old developer mostly interested in web and cloud development. You
-              can see some of my projects on Github.
+              can see some of my projects on my{' '}
+              <Link passHref href="https://github.com/prixladi">
+                <a className="text-red-600 underline hover:no-underline">Github</a>
+              </Link>
+              .
             </motion.p>
           </motion.div>
         </motion.div>
@@ -52,6 +57,11 @@ const Home: NextPage = () => (
           />
         </div>
       </main>
+      <div className="bottom-0 absolute h-20 flex content-center max-w-7xl m-auto">
+        <p className="my-4 text-xl text-center text-gray-500 dark:text-gray-400">
+          Ladislav Prix &copy; 2021, elix78963@gmail.com
+        </p>
+      </div>
     </div>
   </>
 );

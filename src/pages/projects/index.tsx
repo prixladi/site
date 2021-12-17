a143e8bda320700f32d92207ad3bb23c80db2969
Ladislav Prix <ladislav.prix@designeo.cz>
2021-12-17 23:59:51 +0100

    lint fix

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,13 +1,13 @@
 import { motion } from 'framer-motion';
 import type { NextPage } from 'next';
+import Link from 'next/link';
+import { FaGithub } from 'react-icons/fa';
+import { useMemo } from 'react';
 import InlineLink from '../../components/inlineLink';
 import { apearingTextInit, scaleUpHover } from '../../utils/motions';
 import Content from '../../components/content';
 import { defaultScaleUpHoverOptions } from '../../utils/motions/scaleUpHover';
 import projects, { Project } from '../../data/projects';
-import Link from 'next/link';
-import { FaGithub } from 'react-icons/fa';
-import { useMemo } from 'react';
 
 type ProjectProps = {
   project: Project;
@@ -40,15 +40,17 @@ const ProjectTitle = ({ project }: ProjectProps) => {
   return (
     <h3 className="pb-4">
       <Link passHref href={project.link}>
+        {/* eslint-disable */}
         <a className="span items-center text-red-600 font-bold text-2xl hover:underline">
           {project.title} {isGithub ? <FaGithub className="inline-block pb-1" /> : null}
         </a>
+        {/* eslint-enable */}
       </Link>
     </h3>
   );
 };
 
-const Project = ({ project }: ProjectProps) => (
+const ProjectCard = ({ project }: ProjectProps) => (
   <motion.div {...apearingTextInit().children} className="grid grid-cols-2 gap-4">
     <div className="flex">
       <div className="flex flex-col">
@@ -68,40 +70,38 @@ const Project = ({ project }: ProjectProps) => (
   </motion.div>
 );
 
-const Projects: NextPage = () => {
-  return (
-    <Content title="Ladislav Prix | Timeline" className="max-w-4xl">
-      <main className="flex flex-col gap-8 items-center">
-        <motion.div {...apearingTextInit().parent} className="w-full flex flex-col gap-4">
-          <motion.div {...apearingTextInit().children}>
-            <motion.h2
-              {...scaleUpHover()}
-              className="text-3xl md:text-4xl font-semibold duration-700 leading-tight md:leading-[3rem]"
-            >
-              Here are some selected projects I{"'"}ve worked on.
-            </motion.h2>
-          </motion.div>
-          <motion.div {...apearingTextInit().children}>
-            <motion.p
-              {...scaleUpHover()}
-              className="text-xl lg:pl-2 md:text-4xl font-semibold leading-tight md:leading-[3rem] text-gray-500 dark:text-gray-400 duration-700"
-            >
-              They contain brief description and link to repo or site. Most of them you can find on
-              my <InlineLink href="https://github.com/prixladi" text="Github" />.
-            </motion.p>
-          </motion.div>
+const Projects: NextPage = () => (
+  <Content title="Ladislav Prix | Timeline" className="max-w-4xl">
+    <main className="flex flex-col gap-8 items-center">
+      <motion.div {...apearingTextInit().parent} className="w-full flex flex-col gap-4">
+        <motion.div {...apearingTextInit().children}>
+          <motion.h2
+            {...scaleUpHover()}
+            className="text-3xl md:text-4xl font-semibold duration-700 leading-tight md:leading-[3rem]"
+          >
+            Here are some selected projects I&apos;ve worked on.
+          </motion.h2>
         </motion.div>
-        <motion.div
-          {...apearingTextInit({ delayChildren: 1 }).parent}
-          className="w-full flex flex-col gap-20 mt-8"
-        >
-          {projects.map((project) => (
-            <Project key={project.title} project={project} />
-          ))}
+        <motion.div {...apearingTextInit().children}>
+          <motion.p
+            {...scaleUpHover()}
+            className="text-xl lg:pl-2 md:text-4xl font-semibold leading-tight md:leading-[3rem] text-gray-500 dark:text-gray-400 duration-700"
+          >
+            They contain brief description and link to repo or site. Most of them you can find on my{' '}
+            <InlineLink href="https://github.com/prixladi" text="Github" />.
+          </motion.p>
         </motion.div>
-      </main>
-    </Content>
-  );
-};
+      </motion.div>
+      <motion.div
+        {...apearingTextInit({ delayChildren: 1 }).parent}
+        className="w-full flex flex-col gap-20 mt-8"
+      >
+        {projects.map((project) => (
+          <ProjectCard key={project.title} project={project} />
+        ))}
+      </motion.div>
+    </main>
+  </Content>
+);
 
 export default Projects;

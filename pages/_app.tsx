10cb40f27d5de89a7e36f86e0ef1b64d9df68df7
Ladislav Prix <elix78963@gmail.com>
2022-04-22 19:27:59 +0200

    tmp

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,11 @@ import { ThemeProvider } from 'next-themes';
 import type { AppProps } from 'next/app';
 
 import Navbar from '~components/navbar';
-import Kbar from '~/components/kbar'aaa;
+import Kbar from '~/components/kbar'
 
 import '../styles/globals.css';
 import 'react-vertical-timeline-component/style.min.css';
-asdadgit aadasdasdasdad
+
 const App = ({ Component, pageProps }: AppProps) => (
   <ThemeProvider attribute="class" enableSystem={false} defaultTheme="dark">
     <Kbar>asdad
